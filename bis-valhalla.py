4e50053b8bf93fd538173f73a91ec08c383a5824
Holkf <69319985+Holkf@users.noreply.github.com>
2020-08-08 19:45:26 +0200

    Add files via upload
    Einige kleine Verbesserungen

--- bis-valhalla.py
+++ bis-valhalla.py
@@ -1,5 +1,7 @@
+# Es wird die Zeit ausgerechnet bis zum Veröffentlichungstermin von AC Valhalla
 import datetime
 today = datetime.date.today()
 someday = datetime.date(2020,11,17)
 diff = someday - today
 print (diff.days)
+print ("Noch {} Tage bis Assassins Creed Valhalla erscheint".format(diff.days))
